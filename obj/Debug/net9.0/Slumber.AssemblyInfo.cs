fec64e9177bf0464f769a1ea9d61e65cf9bf3716
Turtolo <Lauvstadnils4@gmail.com>
2025-11-03 07:48:57 +0100

    Commit

--- obj/Debug/net9.0/Slumber.AssemblyInfo.cs
+++ obj/Debug/net9.0/Slumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb486c7a230474094b1256c1688f95238d16252d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a11cc2da98c81a151b512944c2cb2b5d030f809")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
