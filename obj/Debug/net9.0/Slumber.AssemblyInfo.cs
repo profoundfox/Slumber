912344896efc39271eda9009e1b98d123a4dac97
Turtolo <lauvstadnils4@gmail.com>
2025-11-16 12:11:56 +0100

    Used the new FromFile function

--- obj/Debug/net9.0/Slumber.AssemblyInfo.cs
+++ obj/Debug/net9.0/Slumber.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c186c146f06c1852d8a193f36ee0f6aef2a1158")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38e33150dfe93561b2e7dd096148be524ac2b8a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Slumber")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
